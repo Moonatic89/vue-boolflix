457a188c271f15cfc8f3cee94fdbc1058e6fa236
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-18 11:49:56 +0100

    TEST v-model entry

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,12 +1,27 @@
 <template>
   <header>
     <label for="chars">Type some characters to search a Movie:</label>
-    <input type="text" id="chars" name="chars" />
+    <input v-model="entry" type="text" id="chars" name="chars" />
+    <button @click="buildAPI(entry)">Search!</button>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      preFixed: "",
+      entry: "",
+      sufFixed: "",
+      stringAPI: "",
+    };
+  },
+  methods: {
+    buildApi() {
+      this.stringAPI = "";
+    },
+  },
+};
 </script>
 
 <style>
