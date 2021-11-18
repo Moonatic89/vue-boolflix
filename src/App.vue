b5686227dd54af70f609b59170b3b58a203db359
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-18 15:17:45 +0100

    FIX - Change approach, now the array generates correctly

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <SiteHeader @getAPI="sendEntry" />
-    <SiteMain :stringEntry="stringEntry" />
+    <SiteHeader @getAPI="getEntry" />
+    <SiteMain :movies="importedAPI" />
 
     <!-- <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
@@ -9,37 +9,36 @@
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
 import SiteHeader from "./components/SiteHeader.vue";
 import SiteMain from "./components/SiteMain.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     SiteHeader,
     SiteMain,
-    // HelloWorld,
   },
   data() {
     return {
       stringEntry: "",
+      importedAPI: [],
+      preFixed:
+        "https://api.themoviedb.org/3/search/movie?api_key=f827a5bacdaf0b2436071ace43764985&language=en-US&query=",
+      sufFixed: "&page=1&include_adult=false",
     };
   },
   methods: {
-    sendEntry(entry) {
-      this.stringEntry = entry;
+    getEntry(entry) {
+      this.stringEntry = this.preFixed + entry + this.sufFixed;
+      axios.get(this.stringEntry).then((r) => {
+        // console.log(r.data.results);
+        this.importedAPI = r.data.results;
+      });
     },
   },
 };
 </script>
 
 <style lang="scss">
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
 </style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -10,36 +10,22 @@
   </main>
 </template>
 
+
 <script>
-import axios from "axios";
 export default {
   props: {
-    stringEntry: String,
+    movies: Array,
   },
 
   data() {
     return {
-      importedAPI: [],
-      completeAPIString: this.getAPI,
-      preFixed:
-        "https://api.themoviedb.org/3/search/movie?api_key=f827a5bacdaf0b2436071ace43764985&language=en-US&query=",
-      sufFixed: "&page=1&include_adult=false",
+      //   importedAPI: [],
     };
-  },
-  computed: {
-    getAPI() {
-      return this.preFixed + this.stringAPI + this.sufFixed;
-    },
-  },
-  mounted() {
-    if (this.stringAPI != "") {
-      axios.get(this.stringAPI).then((r) => {
-        this.importedAPI = r.data.response;
-      });
-    }
   },
 };
 </script>
+
+
 
 <style>
 </style>
