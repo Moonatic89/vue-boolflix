df2000a20315f156f4af45de283249c4bd7282a1
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-18 12:00:01 +0100

    FEATURE - Api build correctly

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <label for="chars">Type some characters to search a Movie:</label>
     <input v-model="entry" type="text" id="chars" name="chars" />
-    <button @click="buildAPI(entry)">Search!</button>
+    <button @click="buildAPI">Search!</button>
   </header>
 </template>
 
@@ -10,15 +10,16 @@
 export default {
   data() {
     return {
-      preFixed: "",
+      preFixed:
+        "https://api.themoviedb.org/3/search/movie?api_key=f827a5bacdaf0b2436071ace43764985&language=en-US&query=",
       entry: "",
-      sufFixed: "",
+      sufFixed: "&page=1&include_adult=false",
       stringAPI: "",
     };
   },
   methods: {
-    buildApi() {
-      this.stringAPI = "";
+    buildAPI() {
+      this.stringAPI = this.preFixed + this.entry + this.sufFixed;
     },
   },
 };
