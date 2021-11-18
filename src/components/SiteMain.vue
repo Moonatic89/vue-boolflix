6bc2839db27588b25a1e80ed4ae17d91cced2158
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-18 15:27:08 +0100

    FEATURE - List show correctly

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -2,10 +2,17 @@
   <main>
     <h1></h1>
     <ol>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <li v-for="movie in movies" :key="movie.id">
+        <ul>
+          <li>Titolo: {{ movie.title }}</li>
+          <li>Titolo Originale: {{ movie.original_title }}</li>
+          <li>Lingua: {{ movie.original_language }}</li>
+          <li>Voto: {{ movie.vote_average }}</li>
+          <br />
+        </ul>
+      </li>
+
+      <ul></ul>
     </ol>
   </main>
 </template>
