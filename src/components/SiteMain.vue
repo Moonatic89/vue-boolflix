1d6f6abc61140af78a41e71c8bdf824af7495150
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-19 11:42:48 +0100

    TEST Flag instead of language code

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -6,7 +6,14 @@
         <ul>
           <li>Titolo: {{ movie.title }}</li>
           <li>Titolo Originale: {{ movie.original_title }}</li>
-          <li>Lingua: {{ movie.original_language }}</li>
+          <li>
+            Lingua:
+            <img
+              class="flag"
+              :src="generateFlag(movie.original_language)"
+              alt=""
+            />
+          </li>
           <li>Voto: {{ movie.vote_average }}</li>
           <br />
         </ul>
@@ -26,13 +33,24 @@ export default {
 
   data() {
     return {
+      flagURL: "https://flagcdn.com/256x192/",
+      formatPNG: ".png",
+      flag: "",
       //   importedAPI: [],
     };
+  },
+  methods: {
+    generateFlag(flag) {
+      return this.flagURL + flag + this.formatPNG;
+    },
   },
 };
 </script>
 
 
 
-<style>
+<style lang="scss">
+.flag {
+  background-color: red;
+}
 </style>
